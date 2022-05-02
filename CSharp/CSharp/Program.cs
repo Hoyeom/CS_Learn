ca46385a414fa0b210cf12f83a1bcf2ff7444d05
Hoyeom <korehdrb@gmail.com>
2022-05-02 20:48:00 +0900

    CS Learn

---
+++ CSharp/CSharp/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+
+namespace CSharp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+
+        #region DATA
+
+        // byte(1바이트 0~255), short(2바이트 -3만~3만), int(4바이트 -21억~21억), long(8바이트)
+        // sbyte(1바이트 -128~127), short(2바이트 -0~6만), int(4바이트 0~43억), long(8바이트)
+
+        // 10진수
+        // 0 1 2 3 4 5 6 7 8 9
+        // 10
+        // 2진수
+        // 0 1
+        // 0b00 0b01 0b10 0b11 0b100
+        // 0b 1000 1111 = 0x8F
+
+        // 16진수
+        // 0~9 a b c d e f
+        // 0x00 0x01 0x02 .. 0x0F
+        // 0x10 == 16
+        // 16진수
+
+        // 1바이트(참/거짓)
+        // bool b;
+        // b = true;
+        // b = false;
+
+        // 소수
+
+        // 4바이트
+        // float f = 3.14f;
+
+        // 8바이트
+        // double d = 3.14;
+        //
+        // char c = 'a';
+        // string str = "Hello World!";
+        //
+        // Console.WriteLine(str);
+
+        // 주석
+        /* 주석 */
+
+        // int hp = 100;
+        // int level = 50;
+        //
+        // bool isAlive = hp > 0;
+        // bool isHighLevel = (level >= 40);
+        //
+        //
+        // bool a = isAlive && isHighLevel;
+        //
+        // bool b = isAlive || isHighLevel;
+
+        // int id = 123;
+        // int key = 401;
+        //
+        // 암호화
+        // int a = id ^ key;
+        // int b = a ^ key;
+        //     
+        // Console.WriteLine(a);
+        // Console.WriteLine(b);
+
+        #endregion
+
+        #region 가위바위보
+
+        /*Random rand = new Random();
+            
+        int aiChoice = rand.Next(0, 3);
+
+        int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+        {
+            case 0:
+            Console.WriteLine("당신의 선택은 가위입니다.");
+            break;
+            case 1:
+            Console.WriteLine("당신의 선택은 바위입니다.");
+            break;
+            case 2:
+            Console.WriteLine("당신의 선택은 보입니다.");
+            break;
+        }
+        switch (aiChoice)
+        {
+            case 0:
+            Console.WriteLine("컴퓨터의 선택은 가위입니다.");
+            break;
+            case 1:
+            Console.WriteLine("컴퓨터의 선택은 바위입니다.");
+            break;
+            case 2:
+            Console.WriteLine("컴퓨터의 선택은 보입니다.");
+            break;
+        }
+            
+        // 승리 무승부 패배
+        string answer = String.Empty;
+            
+            if (choice == aiChoice)
+        answer = "무승부";
+        else if ((choice + 1) % 3 == aiChoice)
+        answer = "패배";
+        else
+        answer = "승리";
+            
+        Console.WriteLine(answer);*/
+
+        #endregion
+
+        #region Magic Mirror
+
+        //  string answer = String.Empty;
+        //      do
+        //  {
+        //      Console.WriteLine("강사님은 잘생기셨나요? (y/n) : ");
+        //      answer = Console.ReadLine();
+        //  } while (answer != "y");
+        //      
+        //   Console.WriteLine("정답입니다!");
+
+        #endregion
+
+        #region Prime
+
+        /*int num = 1000002;
+
+        bool isPrime = true;
+        for (int i = 2; i < num; i++)
+        {
+        if (num % i == 0)
+        {
+            isPrime = false;
+            break;
+            }
+        }
+
+        Console.WriteLine(isPrime ? "소수에요!!" : "소수 아니에요!!");*/
+
+        #endregion
+
+        #region Continue
+
+        // // cw + Tab Console.WriteLine....Me..Mo...메...모.
+        // for (int i = 1; i <= 100; i++)
+        // {
+        //     if ((i % 3) != 0)
+        //         continue;
+        //     
+        //     Console.WriteLine($"3으로 나뉘는 숫자 발견 {i.ToString()}");
+        // }
+
+        #endregion
+    }
+}
