9433e71e4bc164ae74e45f1e7e82fdf02093d0a6
Hoyeom <korehdrb@gmail.com>
2022-05-04 21:18:53 +0900

    Text RPG2

---
+++ CSharp/CSharp/Creature.cs
@@ -0,0 +1,43 @@
+﻿namespace CSharp
+{
+    public enum CreatureType
+    {
+        None,
+        Player = 1,
+        Monster =2 
+    }
+    
+    public class Creature
+    {
+        private CreatureType type;
+
+        protected int hp = 0;
+        protected int attack = 0;
+
+        protected Creature(CreatureType type)
+        {
+            this.type = type;
+        }
+        
+
+        public void SetInfo(int hp, int attack)
+        {
+            this.hp = hp;
+            this.attack = attack;
+        }
+
+        public int GetHP() => hp;
+        public int GetAttack() => attack;
+
+        public bool IsDead() { return hp <= 0;}
+        
+        public void Ondamaged(int damage)
+        {
+            hp -= damage;
+            if (hp < 0)
+                hp = 0;
+        }
+
+        public CreatureType GetCreatureType() => type;
+    }
+}
--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -2,209 +2,33 @@
 
 namespace CSharp
 {
+    // 객체 (OOP Object Oriented Programming
+
+    // Knight
+
+    // 속성: hp, attack
+    // 기능: Move, Attack, Die,
+
+    // OOP ( 은닉성 / 상속성 / 다형성 )
+
+    // 자동차
+    // 핸들 페달 차문
+    // 전기장치 엔진 기름<-> 외부 노출
+
     internal class Program
     {
-        public enum ClassType
+        static void Main(string[] args)
         {
-            None = 0,
-            Knight = 1,
-            Archer = 2,
-            Mage = 3,
-        }
+            Game game = new Game();
 
-        enum MonsterType
-        {
-            None = 0,
-            Slime = 1,
-            Orc = 2,
-            Skeleton = 3,
-        }
-
-        struct Player
-        {
-            public int hp;
-            public int attack;
-        }
-
-        struct Monster
-        {
-            public int hp;
-            public int attack;
-        }
-
-        static ClassType ChooseClass()
-        {
-            int enumLen = Enum.GetValues(typeof(ClassType)).Length;
-
-            Console.WriteLine("직업을 선택하세요!");
-
-            for (int i = 1; i < enumLen; i++)
-                Console.WriteLine($"[{i.ToString()}] {((ClassType) i).ToString()}");
-
-
-            int temp = Convert.ToInt32(Console.ReadLine());
-
-            if (temp > 0 && temp < enumLen)
-            {
-                ClassType classType = (ClassType) temp;
-                Console.WriteLine($"당신은 {classType.ToString()} 입니다~");
-                return classType;
-            }
-
-            return ClassType.None;
-        }
-
-        static void CreatePlayer(ClassType choice, out Player player)
-        {
-            switch (choice)
-            {
-                case ClassType.Knight:
-                    player.hp = 100;
-                    player.attack = 10;
-                    break;
-                case ClassType.Archer:
-                    player.hp = 75;
-                    player.attack = 12;
-                    break;
-                case ClassType.Mage:
-                    player.hp = 50;
-                    player.attack = 15;
-                    break;
-                default:
-                    player.hp = 0;
-                    player.attack = 0;
-                    break;
-            }
-        }
-
-        static void CreateRandomMonster(out Monster monster)
-        {
-            Random rand = new Random();
-            int randMonster = rand.Next(1, 4);
-
-            switch (randMonster)
-            {
-                case (int) MonsterType.Slime:
-                    Console.WriteLine("슬라임이 스폰되었습니다");
-                    monster.hp = 20;
-                    monster.attack = 2;
-                    break;
-                case (int) MonsterType.Orc:
-                    Console.WriteLine("오크가 스폰되었습니다");
-                    monster.hp = 40;
-                    monster.attack = 4;
-                    break;
-                case (int) MonsterType.Skeleton:
-                    Console.WriteLine("스켈레톤이 스폰되었습니다");
-                    monster.hp = 30;
-                    monster.attack = 3;
-                    break;
-                default:
-                    monster.hp = 0;
-                    monster.attack = 0;
-                    break;
-            }
-        }
-
-        static void Fight(ref Player player, ref Monster monster)
-        {
             while (true)
             {
-                // 플레이어가 몬스터 공격
-                monster.hp -= player.attack;
-                if (monster.hp <= 0)
-                {
-                    Console.WriteLine("승리했습니다!");
-                    Console.WriteLine($"남은 체력{player.hp.ToString()}");
-                    break;
-                }
-
-                // 몬스터 반격
-                player.hp -= monster.attack;
-                if (player.hp <= 0)
-                {
-                    Console.WriteLine("패배했습니다!");
-                    break;
-                }
-            }
-        }
-
-        static void EnterField(ref Player player)
-        {
-            while (true)
-            {
-                Console.WriteLine("필드에 접속했습니다!");
-
-                // 몬스터 생성
-                Monster monster;
-                // 랜덤으로 1~3 몬스터 중 하나를 리스폰
-                CreateRandomMonster(out monster);
-
-                Console.WriteLine("[1] 전투 모드로 돌입");
-                Console.WriteLine("[2] 일정 확률로 마을로 도망");
-
-                string input = Console.ReadLine();
-                if (input == "1")
-                {
-                    Fight(ref player, ref monster);
-                }
-                else if (input == "2")
-                {
-                    // 33%
-                    Random rand = new Random();
-                    int randValue = rand.Next(0, 101);
-                    if (randValue <= 33)
-                    {
-                        Console.WriteLine("도망치는데 성공했습니다!");
-                        break;
-                    }
-                    else
-                    {
-                        Fight(ref player, ref monster);
-                    }
-                }
-            }
-        }
-
-        static void EnterGame(ref Player player)
-        {
-            while (true)
-            {
-                Console.WriteLine("마을에 접속했습니다.");
-                Console.WriteLine("[1] 필드로 간다");
-                Console.WriteLine("[2] 로비로 돌아가기");
-
-                string input = Console.ReadLine();
-
-                if (input == "1")
-                {
-                    EnterField(ref player);
-                }
-                else if (input == "2")
-                    break;
-            }
-        }
-
-        public static void Main(string[] args)
-        {
-            while (true)
-            {
-                ClassType choice = ChooseClass();
-                if (choice != ClassType.None)
-                    continue;
-                // 캐릭터 생성
-                Player player;
-
-                CreatePlayer(choice, out player);
-
-                EnterGame(ref player);
-                // 기사(100/10) 궁수(75/12) 법사(50/15)
-
-                // 필드로 가서 몬스터랑 싸운다
+                game.Process();
             }
         }
     }
 }
+
 
 // int ret1 = Add(b: 3, a: 2, d: 5f);  // 이게...C#?... 좋다
 
@@ -413,6 +237,232 @@ Console.WriteLine(isPrime ? "소수에요!!" : "소수 아니에요!!");*/
 //         n :
 //         n * Factorial(n - 1);
 // }
+
+#endregion
+
+#region 절차지향 TEXT RPG
+
+/*public enum ClassType
+{
+    None = 0,
+    Knight = 1,
+    Archer = 2,
+    Mage = 3,
+}
+
+enum MonsterType
+{
+    None = 0,
+    Slime = 1,
+    Orc = 2,
+    Skeleton = 3,
+}
+
+struct Player
+{
+    public int hp;
+    public int attack;
+}
+
+struct Monster
+{
+    public int hp;
+    public int attack;
+}
+
+static ClassType ChooseClass()
+{
+    int enumLen = Enum.GetValues(typeof(ClassType)).Length;
+
+    Console.WriteLine("직업을 선택하세요!");
+
+    for (int i = 1; i < enumLen; i++)
+        Console.WriteLine($"[{i.ToString()}] {((ClassType) i).ToString()}");
+
+
+    int temp = Convert.ToInt32(Console.ReadLine());
+
+    if (temp > 0 && temp < enumLen)
+    {
+        ClassType classType = (ClassType) temp;
+        Console.WriteLine($"당신은 {classType.ToString()} 입니다~");
+        return classType;
+    }
+
+    return ClassType.None;
+}
+
+static void CreatePlayer(ClassType choice, out Player player)
+{
+    switch (choice)
+    {
+        case ClassType.Knight:
+            player.hp = 100;
+            player.attack = 10;
+            break;
+        case ClassType.Archer:
+            player.hp = 75;
+            player.attack = 12;
+            break;
+        case ClassType.Mage:
+            player.hp = 50;
+            player.attack = 15;
+            break;
+        default:
+            player.hp = 0;
+            player.attack = 0;
+            break;
+    }
+}
+
+static void CreateRandomMonster(out Monster monster)
+{
+    Random rand = new Random();
+    int randMonster = rand.Next(1, 4);
+
+    switch (randMonster)
+    {
+        case (int) MonsterType.Slime:
+            Console.WriteLine("슬라임이 스폰되었습니다");
+            monster.hp = 20;
+            monster.attack = 2;
+            break;
+        case (int) MonsterType.Orc:
+            Console.WriteLine("오크가 스폰되었습니다");
+            monster.hp = 40;
+            monster.attack = 4;
+            break;
+        case (int) MonsterType.Skeleton:
+            Console.WriteLine("스켈레톤이 스폰되었습니다");
+            monster.hp = 30;
+            monster.attack = 3;
+            break;
+        default:
+            monster.hp = 0;
+            monster.attack = 0;
+            break;
+    }
+}
+
+static void Fight(ref Player player, ref Monster monster)
+{
+    while (true)
+    {
+        // 플레이어가 몬스터 공격
+        monster.hp -= player.attack;
+        if (monster.hp <= 0)
+        {
+            Console.WriteLine("승리했습니다!");
+            Console.WriteLine($"남은 체력{player.hp.ToString()}");
+            break;
+        }
+
+        // 몬스터 반격
+        player.hp -= monster.attack;
+        if (player.hp <= 0)
+        {
+            Console.WriteLine("패배했습니다!");
+            break;
+        }
+    }
+}
+
+static void EnterField(ref Player player)
+{
+    while (true)
+    {
+        Console.WriteLine("필드에 접속했습니다!");
+
+        // 몬스터 생성
+        Monster monster;
+        // 랜덤으로 1~3 몬스터 중 하나를 리스폰
+        CreateRandomMonster(out monster);
+
+        Console.WriteLine("[1] 전투 모드로 돌입");
+        Console.WriteLine("[2] 일정 확률로 마을로 도망");
+
+        string input = Console.ReadLine();
+        if (input == "1")
+        {
+            Fight(ref player, ref monster);
+        }
+        else if (input == "2")
+        {
+            // 33%
+            Random rand = new Random();
+            int randValue = rand.Next(0, 101);
+            if (randValue <= 33)
+            {
+                Console.WriteLine("도망치는데 성공했습니다!");
+                break;
+            }
+            else
+            {
+                Fight(ref player, ref monster);
+            }
+        }
+    }
+}
+
+static void EnterGame(ref Player player)
+{
+    while (true)
+    {
+        Console.WriteLine("마을에 접속했습니다.");
+        Console.WriteLine("[1] 필드로 간다");
+        Console.WriteLine("[2] 로비로 돌아가기");
+
+        string input = Console.ReadLine();
+
+        if (input == "1")
+        {
+            EnterField(ref player);
+        }
+        else if (input == "2")
+            break;
+    }
+}
+
+// 절차(procedure) 지향
+public static void Main(string[] args)
+{
+    while (true)
+    {
+        ClassType choice = ChooseClass();
+        if (choice != ClassType.None)
+            continue;
+        // 캐릭터 생성
+        Player player;
+
+        CreatePlayer(choice, out player);
+
+        EnterGame(ref player);
+        // 기사(100/10) 궁수(75/12) 법사(50/15)
+
+        // 필드로 가서 몬스터랑 싸운다
+    }
+}*/
+
+#endregion
+
+#region String
+
+/*string name = "Harry Potter";
+
+// 찾기
+bool found = name.Contains("Harry");
+int index = name.IndexOf('P');
+
+// 변형
+name += "Junior";
+
+string lowerCaseName = name.ToLower();
+string upperCaseName = name.ToUpper();
+string newName = name.Replace('r', 'l');
+
+// 분할
+string[] names = name.Split(new char[] { ' ' });
+string subStringName = name.Substring(6);*/
 
 #endregion
 
