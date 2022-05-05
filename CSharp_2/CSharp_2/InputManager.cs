7a446be732b627c164a2fef9a97ad1b60fb1ed67
Hoyeom <korehdrb@gmail.com>
2022-05-06 01:36:14 +0900

    Event Learn

---
+++ CSharp_2/CSharp_2/InputManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CSharp_2
+{
+    // Observer Pattern
+    public class InputManager
+    {
+        public delegate void OnInputKey();
+        public event OnInputKey InputKey;
+        public event Action InputKeyTest;
+        
+        public void Update()
+        {
+            if(!Console.KeyAvailable) return;
+            
+            ConsoleKeyInfo info = Console.ReadKey();
+            if (info.Key == ConsoleKey.A)
+            {
+                // 알려준다! 이벤트로!
+                InputKey?.Invoke();
+                InputKeyTest?.Invoke();
+            }
+        }
+    }
+}
---
+++ CSharp_2/CSharp_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharp_2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
+
+#region EVENT
+
+/*static void OnInputTest()
+{
+    Console.WriteLine("Input Received");
+}
+
+
+public static void Main(string[] args)
+{
+    InputManager inputManager = new InputManager();
+    inputManager.InputKey += OnInputTest;
+    
+    while (true)
+    {
+        inputManager.Update();
+    }
+}*/
+
+#endregion
